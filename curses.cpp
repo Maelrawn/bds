276600b293dbddc7dc8eeed87d56118905685242
James DeLaura <james@Jamess-MacBook-Air.local>
2022-05-14 23:05:53 -0400

    Sick intro graphic; half of functions from previous project converted to use for current project.

--- curses.cpp
+++ curses.cpp
@@ -9,8 +9,8 @@ const float pi = 22/7;
 //Clears the entire screen, so no artifacts are left over after
 //switching contexts.
 void clear_screen(){  
-   for(int x = 0; x < WIDTH; x++)
-      for(int y = 0; y < HEIGHT; y++)
+   for(int x = 0; x <= WIDTH; x++)
+      for(int y = 0; y <= HEIGHT; y++)
          mvaddch(y, x, ' ');
 }
 
@@ -42,10 +42,8 @@ void clear_screen_intro(){
 "                                                                                 ",
 "                                                                                 ",
 "                                                                                 ",
-"                                                                                 ",
-
-						};
-    for(int x = 0; x < WIDTH; x++)
+"                                                                                 ",};
+    for(int x = 0; x <= WIDTH; x++)
         for(int y = 1; y < HEIGHT; y++)
         	if(logo.at(y).at(x) == ' ')
 	           mvaddch(y, x, ' ');
@@ -54,47 +52,44 @@ void clear_screen_intro(){
 //too lazy to figure out a more intelligent way to do this tbh
 
 void intro_graphic(){
-	for(int k = 0; k < 46; k++){
-		for(int i = 0; i < 80; i++){
-			for(int j = 0; j < 25; j++){
-
-				init_color(COLOR_RED, 1000*sin(atan2(2*pi/(.01*i) - k, 2*pi/(.04*j) - k)),
-									  1000*cos(1 - atan2(2*pi/(.04*j) - k, 2*pi/(0.01*i) - k)),
-									  1000*sin(atan(k)));
-				attron(COLOR_PAIR(1));
-				if(i%2 == 0){					
-					mvaddch(j, i, ACS_CKBOARD);
-				}
-				else
-					mvaddch(HEIGHT - j, i, ACS_CKBOARD);
-				attroff(COLOR_PAIR(1));
-				refresh();
-				usleep(3000);
+	int k = 45;
+	for(int i = 0; i < 81; i++){
+		for(int j = 0; j < 25; j++){
+			init_color(COLOR_RED, 11*i,11*i,200+10*i);
+			attron(COLOR_PAIR(1));
+			if(i%2 == 0){					
+				mvaddch(j, i, ACS_CKBOARD);
 			}
-			clear_screen_intro();
+			else
+				mvaddch(HEIGHT - j, WIDTH-i, ACS_CKBOARD);
+			attroff(COLOR_PAIR(1));
+			refresh();
+			usleep(5000);
 		}
-		k += 44;
+		clear_screen_intro();
 	}
+	sleep(2);
+	clear_screen();
 }
 
-/*
-//Same as above but leaves the header and border intact
-void clear_workspace(){
-   for(int x = 5; x < MAXY - 1; x++)
-       for(int y = 1; y < MAXX - 1; y++)
+//clears an arbitrary rectangle, because I was about to have 10 functions
+//that all cleared differently shaped rectangles
+void clear_rectangle(int x1, int x2, int y1, int y2){
+   for(int x = x1; x < x2 - 1; x++)
+       for(int y = y1; y < y2; y++)
           mvaddch(x, y, ' ');
 }
 
 //Outlines our window with a character
-void draw_fullscreen_border(char graphic){
-   for(int x = 0; x < MAXX; x++){   //Top and bottom borders
-      mvaddch(0, x, graphic);
-      mvaddch(24, x, graphic);
+void draw_fullscreen_border(){
+   for(int x = 0; x < WIDTH; x++){   //Top and bottom borders
+      mvaddch(0, x, ACS_CKBOARD);
+      mvaddch(24, x, ACS_CKBOARD);
    }
 
-   for(int y = 0; y < MAXY; y++){   //Left and right borders
-      mvaddch(y, 0, graphic);
-      mvaddch(y, MAXX, graphic);
+   for(int y = 0; y < HEIGHT; y++){   //Left and right borders
+      mvaddch(y, 0, ACS_CKBOARD);
+      mvaddch(y, WIDTH, ACS_CKBOARD);
    }
 }
 
@@ -126,83 +121,56 @@ void clear_option(int option, int column, int maxoption){
 //The thing at the top
 void draw_header(){
    string header = " Business Documentation Software BDS  |  arrows move, esc backs";
-   mvaddch(1, 38, '|');
-   mvaddch(3, 38, '|');
+   mvaddch(1, 40, '|');
+   mvaddch(3, 40, '|');
    mvhline(4, 1, '-', 76);
    mvaddstr(2, 2, header.c_str());
 }
 
 //The following functions display the left half of the screen in some functions.
 void display_main_menu_options(){
-   vector<string> menuitems;
-   menuitems.push_back("Browse Customers");
-   menuitems.push_back("Add Customer");
-   menuitems.push_back("Edit Customer");
-   menuitems.push_back("Delete Records");
-   menuitems.push_back("Save and Quit");
-   menuitems.push_back("Quit Without Saving");
-   for(int i = 0; i < menuitems.size(); i++){
-      mvaddstr(6 + 2*i, 5, menuitems.at(i).c_str());
-   }
+	draw_header();
+    vector<string> menuitems;
+    menuitems.push_back("Browse Customers");
+    menuitems.push_back("Sort By:");
+    menuitems.push_back("Add Customer");
+    menuitems.push_back("Edit Customer");
+    menuitems.push_back("Delete Records");
+    menuitems.push_back("Save and Quit");
+    menuitems.push_back("Quit Without Saving");
+    for(int i = 0; i < menuitems.size(); i++){
+    	mvaddstr(6 + 2*i, 5, menuitems.at(i).c_str());
+    }
 }
 
-//Displays an individual book and all fields, i.e. from browse mode
-void display_browse_book(vector<Book*> library, int idx){
-   display_browse_labels();
-   vector<string> menudata;
-   Book* book = library.at(idx);
-   menudata.push_back(book->getTitle());
-   menudata.push_back(book->getAuthor());
-   menudata.push_back(book->getPublisher());
-   menudata.push_back(book->getDate());
-   menudata.push_back(to_string(book->getPageCount()));
-   menudata.push_back(as_dhm(book->getReadingTime()));
-   menudata.push_back(book->isComplete());
-   for(int i = 0; i < menudata.size(); i++){
-      mvaddstr(6 + 2*i, MAXY, menudata.at(i).c_str());
-   }
+//Displays an individual customer and their info as a header
+void display_customer_header(vector<AllCustomers*> customers, int idx){
+    vector<string> menudata;
+    AllCustomers* customer = customers.at(idx);
+    menudata.push_back(customer->get_firstn());
+    menudata.push_back(customer->get_lastn());
+    menudata.push_back(to_string(customer->get_phone()));
+    menudata.push_back(to_string(customer->get_acc()));
+    menudata.push_back(customer->get_addr());
+    menudata.push_back(customer->get_city());
+    menudata.push_back(customer->get_state());
+    menudata.push_back(to_string(customer->get_zip()));
+	for(int j = 0; j < 2; j++){	   
+   		for(int i = 0; i < menudata.size()/2; i++){
+      		mvaddstr(6 + 2*j, 3+ 15 * i, menudata.at(i).c_str());
+   		}
+   	}
 }
 
-//Displays all titles in the library that will fit on the screen
-void display_purchases(vector<Book*> library, int idx){
-   clear_workspace();
-   int page = idx/9;
-   for(int i = 0; i < 9 && i + page * 9 < library.size(); i++){
-      mvaddstr(6 + 2*i, 5, library.at(page * 9 + i)->getTitle().c_str());
+//Displays all purchases that will fit on the screen
+//TODO: Adjust sizing for header
+void display_purchases(vector<AllPurchases*> purchases, int idx){
+   clear_rectangle(1, WIDTH, 11, HEIGHT);
+   int page = idx/8;
+   for(int i = 0; i < 8 && i + page * 8 < purchases.size(); i++){
+      mvaddstr(12 + 2*i, 5, purchases.at(page * 8 + i)->get_item().c_str());
    }
    refresh();
-}
-
-//Breaks up the excerpt into lines without cutting any words,
-//then displays those lines sequentially. We have a capacity
-//of about 600 characters without adding pagination. The tools to
-//do so are here, but I am stopping now.
-void displayExcerpt(Book* book){
-   string excerpt = book->getExcerpt();
-   vector<string> excerptLines;
-   int startpos = 0;
-   int lastVal = 0;
-   int i = 0;
-   while(excerpt.size() > 70){
-      if(excerpt[i] == ' '){
-         lastVal = i;
-      }
-      if(i == 70){
-         excerptLines.push_back(excerpt.substr(0, lastVal));
-         excerpt = excerpt.erase(0, lastVal);
-         i = 0;
-      }      
-      i++;
-   }
-   excerptLines.push_back(excerpt);
-   clear_workspace();
-   string titlestr = book->getTitle() + " excerpt:";
-   mvaddstr(6, 5, titlestr.c_str());
-   for(int k = 0; k < excerptLines.size(); k++){
-      mvaddstr(8 + 2*k, 5, excerptLines.at(k).c_str());
-   }
-   refresh();
-   getch();
 }
 
 //Handles vertical menus
@@ -288,16 +256,18 @@ void handleHorInput(int maxitem, int &menuitem){
       input = getch();
       mvaddstr(5, 34, clear.c_str());
       switch(input){
-         case '`':
+         case KEY_BACKSPACE: //stackexchange says backspace can output all
+		 case 127:			 //3 of these things depending on platform
+		 case '\b':
             menuitem = -1;
             return;
-         case 'a':
+         case KEY_RIGHT:
             if(menuitem > 0){
                menuitem--;
                return;
             }
          break;
-         case 'd':
+         case KEY_LEFT:
             if(menuitem < maxitem - 1){
                menuitem++;
                return;
@@ -310,17 +280,22 @@ void handleHorInput(int maxitem, int &menuitem){
 
 //Because we used cbreak() to disable text inputs being immediately
 //displayed in the terminal, we need a function to simulate that
-//functionality. Just don't ask me to backspace.
+//functionality. Now featuring backspace!
 string handleTextIn(int column, int row){
    char ch = ' ';
-   int i = -1;
+   int i = 0;
    string str;
-   while(ch != '\n'){
+   while(ch != '\n' && i >= 0){
       ch = getch();
-      i++;
-      if(ch != '\n'){
-         mvaddch(6 + 2 * row, i+  MAXY * column, ch);
+      if(ch == KEY_BACKSPACE || ch == '127' || ch == '\b'){
+      	mvaddch(6 + 2 * row, i + 15 * column, ' ');
+      	str.pop_back();
+       i--;
+      }
+      else if(ch != '\n'){
+         mvaddch(6 + 2 * row, i + 15 * column, ch);
          str.push_back(ch);
+      	 i++;
       }
       refresh();
    }
@@ -335,15 +310,20 @@ string handleNumericIn(int column, int row){
    regex regex (expr);
    char ch = ' ';
    string chstr = " ";
-   int i = -1;
+   int i = 0;
    string str;
-   while(ch != '\n'){
+   while(ch != '\n' && i >= 0){
       ch = getch();
       chstr = ch;
       if(ch != '\n' && regex_match(chstr, regex)){
          i++;
-         mvaddch(6 + 2 * row, i+  MAXY * column, ch);
+         mvaddch(6 + 2 * row, i + 15 * column, ch);
          str.push_back(ch);
+      }
+      else if(ch == KEY_BACKSPACE || ch == '127' || ch == '\b'){
+      	 mvaddch(6 + 2 * row, i + 15 * column, ' ');
+      	 str.pop_back();
+      	 i--;
       }
       refresh();
    }
@@ -352,22 +332,7 @@ string handleNumericIn(int column, int row){
    else
       return to_string(INT_MAX);
 }
-
-//Routine for selecting and displaying chunks of text
-void readBook(vector<Book*> library){
-   int idx = 0;
-   char ch = ' ';
-   while(ch != '`'){
-      display_titles(library, idx);
-      ch = handleVertInputScr(library.size(), 0, idx);
-      if(ch == '\n'){
-         displayExcerpt(library.at(idx));
-         idx = 0;
-         ch = ' ';
-      }
-   }
-}
-
+/*
 //Routine for displaying lots of data about our collection of books
 void browseLibrary(vector<Book*> library){
    int idx = 0;
@@ -375,18 +340,6 @@ void browseLibrary(vector<Book*> library){
       display_browse_book(library, idx);
       handleHorInput(library.size(), idx);
    }
-}
-
-//Takes common ways of saying "true" and standardizes them so we can parse it
-bool stob(string in){
-   for(int i = 0; i < in.size(); i++){
-      in.at(i) = toupper(in.at(i));
-   }
-   if(in == "1" ||in == "TRUE" || in == "YES" || in == "Y"){
-      return 1;
-   }
-   else
-      return 0;
 }
 
 //Creates a new book with user input and puts it into our library
@@ -427,7 +380,7 @@ void editBook(vector<Book*> &library){
          display_browse_book(library, idx);
          option = handleVertInput(7, 0);
          if(option != -1)
-            mvhline(6 + 2 * option, MAXY, ' ', 53);
+            mvhline(6 + 2 * option, HEIGHT, ' ', 53);
          switch(option){
             case 0:
                library.at(idx)->setTitle(handleTextIn(1, option));
@@ -516,17 +469,17 @@ void saveQuit(vector<Book*> library){
 }
 
 //Routine to collect all of the functionality we have built
-void mainMenu(vector<Book*> &library){
+void mainMenu(vector<AllCustomers*> &customers){
    int option = 3;
    while(option != -1){
-      clear_workspace();
-      draw_fullscreen_border('X');
+      clear_rectangle(1, WIDTH, 6, HEIGHT);
+      draw_fullscreen_border();
       draw_header();
       display_main_menu_options();
       refresh();
-      if(library.size() > 0){
+      if(customers.size() > 0){
          option = handleVertInput(7, 0);
-         sortByAuthorAndTitle(library);
+         (library);
       }
       switch(option){
             case -1:
@@ -559,7 +512,6 @@ void mainMenu(vector<Book*> &library){
    return;
 }
 */
-
 void init_curses(){
    initscr();           //This block of stuff starts Curses mode,
    cbreak();            //disables the input buffer (inputs sent immediately after keypress), 
